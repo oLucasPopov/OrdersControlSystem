42a0deadc22e2b33ff15c81fa0a30de454d0d9b4
lucaspopov@protonmail.com <oLucasPopov>
2022-04-02 10:39:54 -0300

    feat: created a helper to fill the cities to a combobox

--- src/Main/Helpers/untAddressHelpers.pas
+++ src/Main/Helpers/untAddressHelpers.pas
@@ -2,11 +2,15 @@ unit untAddressHelpers;
 
 interface
 uses FMX.ListBox, System.Generics.Collections, untGetStateListFactory,
-     untStateModel, untGetStateListController;
+     untStateModel, untGetStateListController, untCityModel,
+     untGetCityListByStateFactory, untGetCityListFromStateController;
 
 type
   TAddressHelpers = class
-    class procedure FeedComboBoxWithStateList(comboBox: TComboBox);
+  public
+    class procedure FeedComboBoxWithStateList(comboBox: TComboBox); static;
+    class procedure FeedComboBoxWithCityListById(comboBox: TComboBox; stateID: Integer); static;
+  private
   end;
 
 implementation
@@ -28,5 +32,23 @@ begin
     getStateListController.Free;
   end;
 end;
+
+class procedure TAddressHelpers.FeedComboBoxWithCityListById(comboBox: TComboBox; stateID: Integer);
+var
+  getCityListFromStateController: TGetCityListFromStateController;
+  cities: TList<TCityModel>;
+  city: TCityModel;
+begin
+  getCityListFromStateController := TGetCityListByState.make;
+  try
+    cities := getCityListFromStateController.list(stateID);
+    comboBox.Items.Clear;
+    for city in cities do
+      comboBox.Items.AddObject(city.city, TObject(city.id));
+  finally
+    getCityListFromStateController.Free;
+  end;
+end;
+
 
 end.
